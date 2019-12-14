6b60fbf4eb691102e43ccc3d106e74acc7ff9e79
Ben <boralben@gmail.com>
2019-12-14 14:13:13 -0600

    upload source maps?

--- scripts/upload-source-maps-sentry.js
+++ scripts/upload-source-maps-sentry.js
@@ -10,12 +10,14 @@ async function createReleaseAndUpload() {
   try {
     console.log('Creating sentry release ' + release);
     await cli.releases.new(release);
+    await cli.releases.setCommits(release, {auto: true});
     console.log('Uploading source maps');
     await cli.releases.uploadSourceMaps(release, {
       include: ['build/static/js'],
       urlPrefix: '~/static/js',
       rewrite: false,
     });
+
     console.log('Finalizing release');
     await cli.releases.finalize(release);
   } catch (e) {

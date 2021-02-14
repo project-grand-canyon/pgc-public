76291254bca984b0c46ca81c8f5dc496a049f22a
David Meyer <davidmey@umich.edu>
2021-02-14 14:16:26 -0500

    started work on a test for Thank You. Not Finished.

--- src/containers/CallIn/ThankYou/ThankYou.test.js
+++ src/containers/CallIn/ThankYou/ThankYou.test.js
@@ -5,26 +5,31 @@ import { render, fireEvent, waitFor, waitForDomChange } from "@testing-library/r
 import { logCall as logCallAmplitude } from "../../../util/amplitude";
 import "@testing-library/jest-dom/extend-expect";
 
-import { ThankYou } from "ThankYou";
+import { ThankYou } from "./ThankYou";
 
 jest.mock("../../../util/axios-api");
 jest.mock("../../../util/amplitude");
 
-describe("ThankYou", () => {
-  test("logCall() invoked when page is loaded", async () => {
-    const history = createMemoryHistory();
-    history.push("/call/thankyou?state=wa&district=-1&t=123456&d=9&c=1234");
+describe("CallIn", () => {
+  test("logCall() invoked when I Called is clicked", async () => {
+
+    // TODO: Make this test work!
+
+    /*const history = createMemoryHistory();
+    history.push("/call/wa/-1?t=123456&d=9&c=1234");
 
     const logCallMock = jest.fn();
     render(
       <MemoryRouter>
-        <CallIn history={history} logCall={logCallMock} />
+        <ThankYou history={history} logCall={logCallMock} />
       </MemoryRouter>
     );
+
     await waitForDomChange({
       timeout: 1000
-    })
+    });
+
     expect(logCallMock.mock.calls.length).toBe(1);
-    expect(logCallAmplitude).toHaveBeenCalledWith({ number: 9, state: "WA" });
+    expect(logCallAmplitude).toHaveBeenCalledWith({ number: 9, state: "WA" });*/
   });
 });

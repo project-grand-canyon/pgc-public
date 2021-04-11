4373a66b0586b0f8d9e77ff6657809b913c47951
Ben <boralben@gmail.com>
2021-04-11 17:48:35 -0500

    lint

--- src/containers/CallIn/CallIn.js
+++ src/containers/CallIn/CallIn.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Button, Col, Collapse, Empty, Icon, List, Row, Spin, Typography } from 'antd';
+import { Button, Col, Collapse, Empty, Icon, List, Row, Spin, Typography } from 'antd';
 import { Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
 

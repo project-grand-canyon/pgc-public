bbab3909f6a9c1a1c8ce05310925519f92fc00d5
Ben <boralben@gmail.com>
2021-05-26 09:24:13 -0500

    swap breadcrumb for error

--- src/containers/CallIn/ThankYou/ThankYou.js
+++ src/containers/CallIn/ThankYou/ThankYou.js
@@ -202,11 +202,7 @@ export class ThankYou extends Component {
                     overallStats: overall
                 })
             }).catch((error) => {
-                Sentry.addBreadcrumb({
-                    category: "Call In Thank You",
-                    message: "Could not fetch stats",
-                    level: Sentry.Severity.Warning,
-                });
+                Sentry.captureException(error);
             });
     }
 
